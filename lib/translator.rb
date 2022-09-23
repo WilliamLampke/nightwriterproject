84daff1eb7c739f6976a004753dff41d0e1b7678
William Lampke <gravitybangers@gmail.com>
2022-09-23 16:30:56 -0400

    Initial commit

---
+++ lib/translator.rb
@@ -0,0 +1,12 @@
+trasnlation_hash = {"a" =>"0.\n..\n..", 
+    "b" => "0.\n0.\n..",
+    "c" => "00\n..\n..",
+    "d" => "00\n.0\n..",
+    "e" => "0.\n.0\n.."
+    "f" => ""
+
+
+
+}
+
+print trasnlation_hash["c"]
