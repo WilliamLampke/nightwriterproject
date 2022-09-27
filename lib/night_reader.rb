e8608cf4ae075a6f2b3facc9cb6591c49952bca0
William Lampke <gravitybangers@gmail.com>
2022-09-26 21:57:25 -0400

    add night_reader

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -4,3 +4,6 @@ first_file = ARGV[0]
 second_file = ARGV[1]
 message = File.open(first_file)
 message_data = File.read(message)
+retranslator = Detranslator.new(message_data)
+File.open(second_file, "w") { |f| f.write retranslator.translate}
+puts "Created '#{second_file}' containing #{retranslator.translate.length} characters"
